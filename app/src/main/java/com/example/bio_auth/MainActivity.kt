333295d1b4639947feb2d3e546f2710697e53db6
imran.a.mohd <emranrasool0201@gmail.com>
2024-02-06 16:35:21 +0530

    "Biometric authentication with lib"

---
+++ app/src/main/java/com/example/bio_auth/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.bio_auth
+
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.biometricauth.BiometricAuthentication
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var biometricAuthBTN: MaterialButton
+    private lateinit var emailEDT: TextInputEditText
+    private lateinit var passwordEDT: TextInputEditText
+    private lateinit var loginButton: MaterialButton
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        uiViewsID()
+        setOnClickEvent()
+
+
+    }
+
+    private fun uiViewsID() {
+        emailEDT = findViewById(R.id.emailEDT)
+        passwordEDT = findViewById(R.id.passwordEDT)
+        loginButton = findViewById(R.id.loginButton)
+        biometricAuthBTN = findViewById(R.id.biometricAuthBTN)
+
+    }
+
+    private fun setOnClickEvent() {
+
+        loginButton.setOnClickListener {
+            val pattern = Patterns.EMAIL_ADDRESS
+            val email = emailEDT.text.toString()
+            val password = passwordEDT.text.toString()
+            if (email.isEmpty() || email.isBlank() || !pattern.matcher(email).matches()) {
+                showMessage("Enter Email")
+                return@setOnClickListener
+            }
+            if (password.isEmpty() || password.isBlank()) {
+                showMessage("Enter Password")
+                return@setOnClickListener
+            }
+
+        }
+        biometricAuthBTN.setOnClickListener {
+            if (BiometricAuthentication.isBiometricSupported(this)) {
+
+                BiometricAuthentication.showBiometricPrompt(this@MainActivity)
+            }
+        }
+
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
---
+++ biometricAuth/src/main/java/com/biometricauth/BiometricAuthentication.kt
@@ -0,0 +1,97 @@
+package com.biometricauth
+
+import android.content.Context
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+
+object BiometricAuthentication {
+
+    //call this method to check biometric support
+    fun isBiometricSupported(context: Context): Boolean {
+        val biometricManager = BiometricManager.from(context)
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
+
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                showMessage(context, ErrorCode.BIOMETRIC_SUCCESS.message)
+                return true
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                showMessage(context, ErrorCode.BIOMETRIC_ERROR_NO_HARDWARE.message)
+                return false
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                showMessage(context, ErrorCode.BIOMETRIC_ERROR_HW_UNAVAILABLE.message)
+                return false
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                showMessage(context, ErrorCode.BIOMETRIC_ERROR_NONE_ENROLLED.message)
+                return false
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> {
+                showMessage(context, ErrorCode.BIOMETRIC_ERROR_UNSUPPORTED.message)
+                return false
+            }
+
+            else -> {
+                showMessage(context, ErrorCode.BIOMETRIC_STATUS_UNKNOWN.message)
+                return false
+            }
+        }
+    }
+
+    //call this method for biometric prompt
+    fun showBiometricPrompt(context: FragmentActivity) {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        val biometricPrompt =
+            BiometricPrompt(context, ContextCompat.getMainExecutor(context),
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        // Handle authentication error
+                        showMessage(context, ErrorCode.AUTHENTICATION_ERROR.message)
+                    }
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        //Handle Authentication Success
+                        showMessage(context, ErrorCode.BIOMETRIC_SUCCESS.message)
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        // Handle authentication failure
+                        showMessage(context, ErrorCode.AUTHENTICATION_FAILED.message)
+                    }
+                })
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun showMessage(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private enum class ErrorCode(val value: Int, val message: String) {
+        BIOMETRIC_SUCCESS(0, "Biometric Success"),
+        BIOMETRIC_STATUS_UNKNOWN(-1, "Biometric status Unknown"),
+        BIOMETRIC_ERROR_UNSUPPORTED(-2, "Biometric Error Unsupported"),
+        BIOMETRIC_ERROR_HW_UNAVAILABLE(1, "Biometric Error Hardware Unavailable"),
+        BIOMETRIC_ERROR_NONE_ENROLLED(11, "Biometric Error None Enrolled"),
+        BIOMETRIC_ERROR_NO_HARDWARE(12, "Biometric Error No Hardware"),
+        AUTHENTICATION_FAILED(13, "Biometric is registered but not authorised with Current User."),
+        AUTHENTICATION_ERROR(14, "Authentication Error");
+
+    }
+}
